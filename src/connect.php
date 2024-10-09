ff8af48e266531ad43025f74bd3368c401431069
newqo <worawit.forwork@gmail.com>
2024-10-09 12:18:18 +0700

    commit pdo

--- src/connect.php
+++ src/connect.php
@@ -1,7 +1,6 @@
 <?php
 try {
-    $pdo = new PDO("host=j00ko0ksw4gckc0gg00oo04o;dbname=reservation_db;charset=utf8", "root", "nyGkqbZmSoTbN8WCJ1aow9B2qDFZnxHJUKkEf3NZz0CZarAk9dBYYBrjVCdmi5rG");
+    $pdo = new PDO("mysql:host=j00ko0ksw4gckc0gg00oo04o;dbname=reservation_db", "root", "nyGkqbZmSoTbN8WCJ1aow9B2qDFZnxHJUKkEf3NZz0CZarAk9dBYYBrjVCdmi5rG");
 } catch (PDOException $e) {
     echo "เกิดข้อผิดพลาด : " . $e->getMessage();
 }
-?>
